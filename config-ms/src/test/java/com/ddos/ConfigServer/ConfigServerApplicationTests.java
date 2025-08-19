57a4c6ff193aa6100bfa47b4caa1fada4a1357ff
fedan <valayevfedya2005@gmail.com>
2025-08-19 20:43:27 +0300

    fix: update config server dependencies and settings

---
+++ config-ms/src/test/java/com/ddos/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddos.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
